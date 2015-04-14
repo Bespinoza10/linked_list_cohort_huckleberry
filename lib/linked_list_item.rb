a6f855ee8aec852889487647a34931c3d37de944
Brandon Espinoza <espinozab100@gmail.com>
2015-04-14 14:11:31 -0500

    Implementation of Linked Lists

--- lib/linked_list_item.rb
+++ lib/linked_list_item.rb
@@ -1,8 +1,32 @@
 class LinkedListItem
-  include Comparable
-  attr_reader :payload
+  include Comparable  #Look into Comparable...
 
-  def initialize(data)
-    @payload = data
+#   attr_accessor :payload, :next_item
+  attr_reader :payload
+  attr_reader :next_item
+
+  def initialize(payload)
+    @payload = payload
+  end
+
+  def last?
+    next_item.nil?    
+  end  
+  
+  def next_item=(lli)
+    raise ArgumentError, "cant next_item its self" if self == lli
+    @next_item = lli
+  end
+  
+  def <=> (other)
+    if self.payload.class == other.payload.class
+      self.payload <=> other.payload
+    else
+      self.payload.class.to_s <=> other.payload.class.to_s
+    end
+  end
+  
+  def === (other)                         
+    self.equal? other
   end
 end
